946406058243d5d1a84b630a8f71a80261dc08ba
Jaskirat132 <jaskirat.gamer.best@gmail.com>
2026-05-01 14:36:45 +0530

    im so dumb

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+from config import app, db
+from models import User
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        
+    app.run(debug=True)
---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from config import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def json_ify(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+        }
